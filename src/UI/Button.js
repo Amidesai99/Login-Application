05e97a39603098b635caed00806437bd0d5353e4
Amidesai99 <72565845+Amidesai99@users.noreply.github.com>
2022-06-27 15:47:51 -0400

    Managed User Input State

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -2,4 +2,12 @@ import React from 'react';
 
 import classes from './Button.module.css';
 
-const
+const Button = props =>{
+    return (
+        <button className={classes.button} type={props.type || 'button'} onClick={props.onClick}>
+            {props.children}
+        </button>
+    )
+}
+
+export default Button;
